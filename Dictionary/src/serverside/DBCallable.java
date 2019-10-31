5beb5ed001d4a128ae650c0440a9b4f61d7deb81
VitalikOrlioglo <vitalik.orlioglo@gmail.com>
2019-10-31 21:50:45 +0100

    Project 2 Modul

--- Dictionary/src/serverside/DBCallable.java
+++ Dictionary/src/serverside/DBCallable.java
@@ -46,7 +46,7 @@ public class DBCallable implements Callable<DictionaryObject>{
 			
 			log.info("DBCallable : preparedstatements & connection created");
 		} catch (SQLException e) {
-			log.error("DBCallable: DB Connection troubles \n" + e.getMessage());
+			log.error("DBCallable: DB Connection/Driver loading troubles \n" + e.getMessage());
 			System.exit(1);
 		}	
 	}
@@ -63,13 +63,16 @@ public class DBCallable implements Callable<DictionaryObject>{
 	 * @return DictionaryObject from DB
 	 */
 	@Override
-	public DictionaryObject call() throws Exception {
+	public DictionaryObject call() {
 		log.info("DBCallable : call method in DBCallable starts");
-		DictionaryObject dictionaryObjectFrom;
-		dictionaryObjectFrom = selector(dictionaryObject);
-		log.debug("DBCallable : answer in call method from method selector received." + "DictionaryObject is: "+
-				"Flag - " + dictionaryObjectFrom.flag + " Word - " + dictionaryObjectFrom.word + " Def - " + dictionaryObjectFrom.definition);
-		
+		DictionaryObject dictionaryObjectFrom = null;
+		try {
+			dictionaryObjectFrom = selector(dictionaryObject);
+			log.debug("DBCallable : answer in call method from method selector received." + "DictionaryObject is: "+
+					"Flag - " + dictionaryObjectFrom.flag + " Word - " + dictionaryObjectFrom.word + " Def - " + dictionaryObjectFrom.definition);
+		} catch (InterruptedException e) {
+			log.error("DBCallable: Thread.sleep(); troubles \n" + e.getMessage());
+		}
 		log.info("DBCallable: DictionaryObject from call method in DBCallable returning to ServerRunnable");
 		return dictionaryObjectFrom;
 	}
@@ -87,9 +90,81 @@ public class DBCallable implements Callable<DictionaryObject>{
 	 * @param dictionaryObject
 	 * @return
 	 */
-	private DictionaryObject selector(DictionaryObject dictionaryObject) {
-		return null;
+	private DictionaryObject selector(DictionaryObject dictionaryObject) throws InterruptedException {
+		log.info("DBCallable : selector works");
+		DictionaryObject dicIn = dictionaryObject;
+		DictionaryObject dicOut = null;
+		int flag = dictionaryObject.flag;
+
+		log.debug("DBCallable: input dictionaryObject in selector in DBCallable: "
+				+ "Flag - " + dicIn.flag + " Word - " + dicIn.word + " Def - " + dicIn.definition);
+		Thread.sleep(5000);
+		switch (flag) {
+			case -1:
+				log.info("DBCallable: DELETE - flag in selector in DBCallable, try to delete entry");
+				log.info("DBCallable: deleting entry with word - " + dicIn.getWord());
+				dicOut = delete(dicIn);
+				break;
+			case 0:
+				log.info("DBCallable: SEARCH - flag in selector in DBCallable, try to SEARCH entry");
+				log.info("DBCallable: searching entry with word - " + dicIn.getWord());
+				dicOut = search(dicIn);
+				log.debug("DBCallable: return into method selector from method  search: "
+						+ "Founded Flag - " + dicOut.flag + " Founded Word - " + dicOut.word + " Founded Def - " + dicOut.definition);
+				break;
+			case 1:
+				log.info("DBCallable: ADD - flag in selector in DBCallable, try to ADD entry");
+				log.debug("DBCallable : adding entry with word - " + dicIn.word);
+				dicOut = add(dicIn);
+				break;
+			case 10:
+				log.info("DBCallable: EDIT - flag in method selector in DBCallable, try to EDIT");
+				log.debug("DBCallable: editing entry with word - " + dicIn.word);
+				dicOut = edit(dicIn);
+				break;
+		}
+
+		log.debug("DBCallable: ready to return answer from method selector to call method. dictionaryObject(dicOut): "
+				+"Flag - "+ dicOut.flag + " Word - " + dicOut.word + " Def - " + dicOut.definition);
+		return dicOut;
 	}
-	
-	
+
+
+	private DictionaryObject edit(DictionaryObject dicIn) {
+		DictionaryObject dicEdit;
+		return dicEdit;
+	}
+
+	private DictionaryObject add(DictionaryObject dicIn) {
+		DictionaryObject dicAdd;
+		return dicAdd;
+	}
+
+	private DictionaryObject search(DictionaryObject dicIn) {
+		DictionaryObject dicSearch;
+		return dicSearch;
+	}
+
+	/**
+	 * SQL запрос к таблице <code>"word_define"</code> в базе данных с именем - <code>"dictionary"</code>, в случае
+	 * удачного удаления, выполняется код в фигурных скобках в которых происходит <code>return </code>преобразованного
+	 * <code>EntryDic</code> в котором флаг операции <code>flag</code> не изменяется, а в полях <code>word </code>и
+	 * <code>definition</code> пишется "ок " - подтверждение удачного выполнения удаления, в случае если удаление не
+	 * произошло то выполнение из круглых скобок не попадает в фигурные, а падает в (<code>SQLException</code>)
+	 * ниже , в котором происходит формирование нового (<code>new EntryDic();</code>)в котором флаг операции
+	 * не меняется, а в полях word и definition выводится - error.
+	 * <p>
+	 * Предусмотрено логирование аварийного завершения выполнения операции в файл <code>"DBServerHandler_error_log.txt"</code>
+	 * <code>file</code> переменная.
+	 * @param dicIn
+	 * @return
+	 */
+	private DictionaryObject delete(DictionaryObject dicIn) {
+		log.debug("DBCallable : starts delete method");
+		DictionaryObject dicDelete;
+
+		return dicDelete;
+	}
+
+
 }
